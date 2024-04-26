2fb94531b6ed03d1bfd223f0c6b53ec29c99b35d
LCostaF <146568540+LCostaF@users.noreply.github.com>
2024-04-26 15:16:17 -0300

    feat: Criar página - Básico
    Adiciona de modo simples a funcionalidade para criar uma "página" para um waffle, gerando um arquivo texto e imagem nos respectivos diretórios. As rotas verificam se existe já um arquivo com o nome selecionado para a página/arquivo, interrompendo a função de Criar caso já exista.

--- controls/routes-main.js
+++ controls/routes-main.js
@@ -1,8 +1,17 @@
 // ---- Arquivo para rotas principais do sistema ----
 
 const cookieParser = require('cookie-parser');
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const formidable = require('formidable');
+const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+
+// ------ MIDDLEWARES ------
+
+
+
+// ------ ROTAS ------
 
 // --- Essas rotas iniciais não exigem que o usuário esteja logado ---
 
@@ -29,6 +38,57 @@ router.get("/novo-waffle", (req, res) => {
     res.render("novo-waffle");
 });
 
+// Rota para tratar a criação de um novo waffle, após preenchido form
+router.post("/criar-waffle", (req, res) => {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            console.error('Error parsing form data:', err);
+            res.status(500).send('Error parsing form data.');
+            return;
+        }
+
+        // Constantes recebem nome e conteúdo de um waffle
+        const fileName = fields.fileName[0];
+        const pageContent = fields.pageContent[0];
+        const filePath = path.join(__dirname, '../waffles/', fileName + '.txt');
+
+        // Verifica se já existe um waffle com o nome de arquivo (rota) fornecido
+        if (fs.existsSync(filePath)) {
+            console.error('Error: A text file with the same name already exists.');
+            res.status(400).send('A text file with the same name already exists.');
+            return;
+        } else {
+            // Path da imagem
+            const imagePath = path.join(__dirname, '../images/', fileName + path.extname(
+                path.join(__dirname, '../images/', String(files.file[0].originalFilename))
+            ));
+
+            // Envia imagem para diretório images
+            fs.copyFile(String(files.file[0].filepath), imagePath, (err) => {
+                if (err) {
+                    console.error('Error moving image file:', err);
+                    res.status(500).send('Error moving image file.');
+                    return;
+                }
+                console.log('Image file saved successfully:', imagePath);
+
+                // Write the content of the waffle to a text file
+                fs.writeFile(filePath, String(pageContent), (err) => {
+                    if (err) {
+                        console.error('Error writing text file:', err);
+                        res.status(500).send('Error writing text file.');
+                        return;
+                    }
+                    console.log('Text file saved successfully:', filePath);
+                    res.redirect("/");
+                });
+            });
+        }
+    });
+});
+
 // Rota para a edição dos dados de um waffle
 router.get("/editar-waffle/:waffle", (req, res) => {
     res.render("editar-waffle");
@@ -40,4 +100,9 @@ router.get("/excluir-waffle/:waffle", (req, res) => {
     res.redirect("/");
 });
 
-module.exports = router
+// Rota para logout
+router.get("/logout", (req, res) => {
+    
+});
+
+module.exports = router;
